eb7243ae12485099a7b2e5d63f19673a9e1bee94
KARUPPASAMYHARIHARAN . A <86235041+karuppasamyhari@users.noreply.github.com>
2023-07-26 14:50:41 +0530

    Add files via upload

---
+++ external.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+function changeCicleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
